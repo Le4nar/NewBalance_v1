626eaa61a41fc9a561e4b43d08f10820230de6d8
Le4nar <71175736+Le4nar@users.noreply.github.com>
2024-02-12 19:04:21 +0200

    19:03 понедельник, 12 февраля 2024 г

--- app/src/main/java/com/hfad/newbalance/CategoryFragment.java
+++ app/src/main/java/com/hfad/newbalance/CategoryFragment.java
@@ -1,59 +1,56 @@
 package com.hfad.newbalance;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class CategoryFragment extends Fragment {
-//    MenCategoryFragment menCategoryFragment=new MenCategoryFragment();
-//    WomenCategoryFragment womenCategoryFragment= new WomenCategoryFragment();
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        return inflater.inflate(R.layout.fragment_category, container, false);
-//    }
-//
-//    @Override
-//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//
-//        ImageView imageViewWomenCategory = view.findViewById(R.id.imageWomenCategory);
-//        ImageView imageViewMenCategory = view.findViewById(R.id.imageMenCategory);
-//        imageViewWomenCategory.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Код для перехода на другой фрагмент
-//                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//
-//                fragmentTransaction.replace(R.id.bottom_navigation, womenCategoryFragment);
-//                fragmentTransaction.addToBackStack(null); // Если нужно добавить транзакцию в стек возврата
-//                fragmentTransaction.commit();
-//
-//            }
-//        });
-//        imageViewMenCategory.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Код для перехода на другой фрагмент
-//                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//
-//                fragmentTransaction.replace(R.id.bottom_navigation, menCategoryFragment);
-//                fragmentTransaction.addToBackStack(null); // Если нужно добавить транзакцию в стек возврата
-//                fragmentTransaction.commit();
-//            }
-//        });
-//    }
+
+    private ImageView ImageWomenCategory;
+    private ImageView ImageMenCategory;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+
+        // Найти картинку
+        ImageWomenCategory = view.findViewById(R.id.imageWomenCategory);
+        ImageMenCategory = view.findViewById(R.id.imageMenCategory);
+
+
+        // Добавить слушателя события onClick() к картинке
+        ImageWomenCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Заменить текущий фрагмент на новый фрагмент
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container, new WomanCategory());
+                fragmentTransaction.commit();
+            }
+        });
+
+        ImageMenCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Заменить текущий фрагмент на новый фрагмент
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.container, new ManCategory());
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
+    }
 }
